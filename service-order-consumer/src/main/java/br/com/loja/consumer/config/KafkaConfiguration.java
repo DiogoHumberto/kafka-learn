bbb6c95e68d5fd2dfb4283bf46e94df2d3719b46
Diogo Humberto <diogo_humberto@hotmail.com>
2023-12-09 21:19:26 -0300

    impl retry caso de erro kafka

--- service-order-consumer/src/main/java/br/com/loja/consumer/config/KafkaConfiguration.java
+++ service-order-consumer/src/main/java/br/com/loja/consumer/config/KafkaConfiguration.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +14,11 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import br.com.loja.consumer.dto.PedCompDto;
 import lombok.RequiredArgsConstructor;
@@ -37,8 +43,10 @@ public class KafkaConfiguration {
 
 		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
 		config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
-		  config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-		    config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+		config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");// pega as msg armazenadas - mesmo antes da aplicação subir
+		//config.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100); // configurar pool de dados rede - padrao 500
+		//config.put(ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG, false); //nao permite auto create de topic 
+		config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
 		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
 		return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), deserializer);
@@ -50,4 +58,21 @@ public class KafkaConfiguration {
 		factory.setConsumerFactory(pedCompConsumerFactory());
 		return factory;
 	}
+	
+    @Bean
+    public ProducerFactory<String, PedCompDto> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+
+    @Bean
+    public KafkaTemplate<String, PedCompDto> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
--- service-order-consumer/src/main/java/br/com/loja/consumer/listener/KafkaConsumer.java
+++ service-order-consumer/src/main/java/br/com/loja/consumer/listener/KafkaConsumer.java
@@ -1,9 +1,11 @@
 package br.com.loja.consumer.listener;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 import br.com.loja.consumer.dto.PedCompDto;
@@ -14,6 +16,8 @@ import lombok.extern.slf4j.Slf4j;
 public class KafkaConsumer {
 	
 	@KafkaListener(topics = "NOVO_PEDCOMP", groupId = "group_json", containerFactory = "pedCompKafkaListenerFactory")
+	@RetryableTopic(backoff = @Backoff(value = 3000L), attempts = "5", autoCreateTopics = "true", include = Exception.class)
+	//configurado para retentativa em caso de erro do tipo Exception
 	public void consumeJson(@Payload PedCompDto pedComp, @Headers MessageHeaders headers) {
 		log.info("-------------------------> Consumed JSON Message, numpeComp {} com o valor {} para emtrega em {}: ",
 				pedComp.getNumpedcomp(), pedComp.getVlTotal(), pedComp.getDtEntrega());
